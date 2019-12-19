2000befccca31fd1c9419e49af5b9254c3bdc01c
someone <someone>
2019-12-18 19:34:06 -0600

    Renamed files

--- PencilDurability/PencilDurability/Pencil.cs
+++ PencilDurability/PencilDurability/Pencil.cs
@@ -2,7 +2,7 @@
 
 namespace PencilDurability
 {
-    public class Class1
+    public class Pencil
     {
     }
 }
--- PencilDurability/PencilDurabilityTests/PencilTests.cs
+++ PencilDurability/PencilDurabilityTests/PencilTests.cs
@@ -3,7 +3,7 @@ using Xunit;
 
 namespace PencilDurabilityTests
 {
-    public class UnitTest1
+    public class PencilTests
     {
         [Fact]
         public void Test1()
